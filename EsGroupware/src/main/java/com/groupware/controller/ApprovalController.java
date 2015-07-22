cacc96e531d913aae833ba2814d459e7caef3e2d
Administrator <Administrator@MSDN-SPECIAL>
2015-07-22 15:47:10 +0900

    approval

---
+++ EsGroupware/src/main/java/com/groupware/controller/ApprovalController.java
@@ -0,0 +1,27 @@
+package com.groupware.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.groupware.dao.EmployeeDao;
+
+
+@Controller
+@RequestMapping(value="approval")
+public class ApprovalController {
+	private EmployeeDao employeeDao;
+	
+	@Autowired
+	@Qualifier("approvalDao")
+	public void setEmployeeDao(EmployeeDao employeeDao) {
+		this.employeeDao = employeeDao;
+	}
+
+	@RequestMapping(value="admin.action", method = RequestMethod.GET)
+	public String admin() {	
+		return "admin/admin";	
+	}
+}
---
+++ EsGroupware/src/main/java/com/groupware/dto/Approval.java
@@ -0,0 +1,17 @@
+package com.groupware.dto;
+
+public class Approval {
+	private	String title;
+	private	String content;
+	private	String reportDate;
+	private	String approvalDate;
+	private	String deadlineDate;
+	private	String releaseWhether;
+	private	String receiveDepartment;
+	private	int retention;
+	private	String department;
+	private	String cooperation;
+	private	String referrers;
+	private	String approvalCheck;
+	private String drafter;
+}
